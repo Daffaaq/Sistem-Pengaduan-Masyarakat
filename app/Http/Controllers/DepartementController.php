bcc35c3bd0ad7e48bacb6d87b76ef0617f05f57f
Daffaaq <daffaaqila48@gmail.com>
2023-07-31 13:43:08 +0700

    request departement and user

--- app/Http/Controllers/DepartementController.php
+++ app/Http/Controllers/DepartementController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Departements;
+use App\Http\Requests\DepartementRequest;
 use Illuminate\Support\Facades\Validator;
 
 class DepartementController extends Controller
@@ -19,37 +20,43 @@ class DepartementController extends Controller
         return view('superadmin.departements.tambah-departements');
     }
 
-    public function store(Request $request)
+    // public function store(Request $request)
+    // {
+    //     // Validate the input data
+    //     $validator = Validator::make($request->all(), [
+    //         'name' => 'required|unique:departements,name', // Ensure that 'name' is unique in the 'departements' table
+    //         'longitude' => 'nullable',
+    //         'latitude' => 'nullable',
+    //     ]);
+
+    //     // Check if validation fails
+    //     if ($validator->fails()) {
+    //         return redirect()->back()
+    //             ->withErrors($validator)
+    //             ->withInput();
+    //     }
+
+    //     // Validation passed, create the department
+    //     Departements::create([
+    //         'name' => $request->name,
+    //         'longitude' => $request->longitude,
+    //         'latitude' => $request->latitude,
+    //     ]);
+
+    //     return redirect()->route('superadmin.departement.index')->with('success', 'Departement created successfully.');
+    // }
+    public function store(DepartementRequest $request)
     {
-        // Validate the input data
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|unique:departements,name', // Ensure that 'name' is unique in the 'departements' table
-            'longitude' => 'nullable',
-            'latitude' => 'nullable',
-        ]);
+        // $validatedData = $request->validated();
+        Departements::create($request->validated());
 
-        // Check if validation fails
-        if ($validator->fails()) {
-            return redirect()->back()
-                ->withErrors($validator)
-                ->withInput();
-        }
-
-        // Validation passed, create the department
-        Departements::create([
-            'name' => $request->name,
-            'longitude' => $request->longitude,
-            'latitude' => $request->latitude,
-        ]);
-
-        return redirect()->route('superadmin.departement.index')->with('success', 'Departement created successfully.');
-        // $data = $request->validate([
-        //     'name' => 'required',
+        // Departements::create([
+        //     'name' => $validatedData['name'],
+        //     'longitude' => $validatedData['longitude'],
+        //     'latitude' => $validatedData['latitude'],
         // ]);
 
-        // Departements::create($data);
-
-        // return redirect()->route('superadmin.departement.index')->with('success', 'Departement created successfully.');
+        return redirect()->route('superadmin.departement.index')->with('success', 'Departement created successfully.');
     }
 
     public function edit($id)
@@ -58,14 +65,21 @@ class DepartementController extends Controller
         return view('superadmin.departements.edit-departements', compact('departement'));
     }
 
-    public function update(Request $request, $id)
-    {
-        $data = $request->validate([
-            'name' => 'required',
-        ]);
+    // public function update(Request $request, $id)
+    // {
+    //     $data = $request->validate([
+    //         'name' => 'required',
+    //     ]);
 
+    //     $departement = Departements::findOrFail($id);
+    //     $departement->update($request->only('name'));
+
+    //     return redirect()->route('superadmin.departement.index')->with('success', 'Departement updated successfully.');
+    // }
+    public function update(DepartementRequest $request, $id)
+    {
         $departement = Departements::findOrFail($id);
-        $departement->update($request->only('name'));
+        $departement->update($request->validated());
 
         return redirect()->route('superadmin.departement.index')->with('success', 'Departement updated successfully.');
     }
