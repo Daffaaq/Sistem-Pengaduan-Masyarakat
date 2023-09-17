887a6d5ace122593d435dbb498194850ca0f1c15
Daffaaq <daffaaqila48@gmail.com>
2023-09-17 22:42:04 +0700

    add format answeComplaint

--- resources/views/admin/complaints/create_answer.blade.php
+++ resources/views/admin/complaints/create_answer.blade.php
@@ -22,20 +22,20 @@
             <h2 class="h3 text-center mb-4">Tambah Jawaban Pengaduan</h2>
             <div class="mb-3">
                 <label for="complaint_title" class="form-label">Judul Pengaduan</label>
-                <input type="text" class="form-control" id="complaint_title" value="{{ $complaint->title }}" readonly>
+                <input type="text" class="form-control" id="complaint_title" value="{{ $complaint->title }}"
+                    readonly>
             </div>
             <div class="mb-3">
                 <label for="complaint_description" class="form-label">Deskripsi Pengaduan</label>
-                <textarea class="form-control" id="complaint_description" rows="5"
-                    readonly>{{ $complaint->description }}</textarea>
+                <textarea class="form-control" id="complaint_description" rows="5" readonly>{{ $complaint->description }}</textarea>
             </div>
             <form action="{{ route('admin.complaints.answer.store', $complaint->id) }}" method="POST"
                 enctype="multipart/form-data">
                 @csrf
                 <div class="mb-3">
                     <label for="answer" class="form-label">Jawaban</label>
-                    <textarea class="form-control @error('answer') is-invalid @enderror" id="answer" name="answer"
-                        rows="5" required></textarea>
+                    <textarea class="form-control @error('answer') is-invalid @enderror" id="answer" name="answer" rows="5"
+                        required></textarea>
                     @error('answer')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -50,6 +50,23 @@
         </div>
     </div>
 
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var answerTextarea = document.getElementById('answer');
+            var jamSekarang = new Date().getHours();
+
+            if (jamSekarang >= 0 && jamSekarang < 12) {
+                var salam = "Selamat Pagi";
+            } else if (jamSekarang >= 12 && jamSekarang < 17) {
+                var salam = "Selamat Siang";
+            } else {
+                var salam = "Selamat Malam";
+            }
+
+            var format = "Assalamualaikum Wr. Wb\n" + salam + "\n\n\n\nTerimakasih Wasalamuaalikum Wr. Wb";
+            answerTextarea.value = format;
+        });
+    </script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
