99e081e84db23a28841828c35d9e31691021382d
Daffaaq <daffaaqila48@gmail.com>
2023-09-21 19:07:01 +0700

    add explain in the UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function edit()
     {
-        // Get the currently authenticated user
+        // Mendapatkan pengguna yang saat ini terotentikasi
         $user = auth()->user();
 
         // Tampilkan halaman edit untuk pengguna dengan role "user"
@@ -19,18 +19,24 @@ class UserController extends Controller
 
     public function update(userUpdateRequest $request)
     {
+        // Mendapatkan data yang valid dari permintaan (request) pengguna
         $data = $request->validated();
 
+        // Memeriksa apakah ada perubahan password dan mengenkripsi password baru jika ada
         if (isset($data['password'])) {
             $data['password'] = bcrypt($data['password']);
         }
+
+        // Mengatur peran pengguna sebagai "user"
         $data['role'] = 'user';
 
-        // Get the currently authenticated user
+        // Mendapatkan pengguna yang saat ini terotentikasi
         $user = auth()->user();
 
+        // Memperbarui data profil pengguna dengan data yang telah diubah
         $user->update($data);
 
+        // Mengarahkan pengguna ke halaman dasbor pengguna dengan pesan sukses
         return redirect()->route('user.dashboard')->with('success', 'Profil berhasil diperbarui.');
     }
 
