99c05f28704cd3547991ff8e191bab95a9c36c2f
Daffaaq <daffaaqila48@gmail.com>
2023-09-26 16:22:48 +0700

    add explain forgot password

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -7,22 +7,27 @@ use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
+    // Menampilkan formulir lupa password
     public function showForgotForm()
     {
         return view('auth.forgotPassword');
     }
 
+    // Meng-handle permintaan lupa password
     public function handleForgot(Request $request)
     {
+        // Validasi input dari formulir
         $request->validate([
         'email' => 'required|email',
         'name' => 'required',
     ]);
 
+    // Mencari pengguna berdasarkan email dan nama dari input
     $user = User::where('email', $request->email)->where('name', $request->name)->first();
 
+    // Jika pengguna tidak ditemukan
     if (!$user) {
-        // Jika user tidak ditemukan
+        // Jika email ada dalam database tetapi nama tidak sesuai
         if (User::where('email', $request->email)->exists()) {
             // Jika email ada dalam database tetapi nama tidak sesuai
             return redirect()->back()->withErrors([
@@ -41,7 +46,7 @@ class ForgotPasswordController extends Controller
             ]);
         }
     } elseif (!in_array($user->role, ['superadmin', 'user'])) {
-        // Jika rolenya tidak antara "superadmin" dan "user"
+        // Jika pengguna ditemukan tetapi rolenya tidak sesuai dengan "superadmin" atau "user"
         return redirect()->back()->withErrors([
             'name' => 'Informasi role tidak valid atau tidak memiliki izin untuk merubah password.',
         ]);
@@ -51,27 +56,34 @@ class ForgotPasswordController extends Controller
         return redirect()->route('password.reset', $user->email);
     }
 
+    // Menampilkan formulir reset password
     public function showResetForm(Request $request)
     {
         return view('auth.resetPassword', ['email' => $request->email]);
     }
 
+    // Meng-handle permintaan reset password
     public function handleReset(Request $request)
     {
+        // Validasi input dari formulir reset password
         $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|confirmed|min:8'
+            'email' => 'required|email', // Email harus diisi dan valid
+            'password' => 'required|confirmed|min:8' // Password harus diisi, cocok dengan konfirmasi, dan minimal 8 karakter
         ]);
 
+        // Mencari pengguna berdasarkan email
         $user = User::where('email', $request->email)->first();
 
+        // Jika pengguna tidak ditemukan atau rolenya tidak sesuai
         if (!$user || !in_array($user->role, ['superadmin', 'user'])) {
             return redirect()->back()->withErrors(['email' => 'Email tidak valid atau tidak memiliki izin.']);
         }
 
+        // Mengubah password pengguna dengan password baru yang di-hash
         $user->password = bcrypt($request->password);
         $user->save();
 
+        // Redirect ke halaman login dengan pesan sukses
         return redirect()->route('login')->with('status', 'Password berhasil diubah!');
     }
 }
